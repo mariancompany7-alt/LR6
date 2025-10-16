068cce1068180a7725cd5d020e4cc0249dce4511
Marian0954 <mariancompany7@gmail.com>
2025-10-16 21:16:05 +0300

    Update grupa.h

--- grupa.h
+++ grupa.h
@@ -5,20 +5,23 @@
 using namespace std;
 
 class Grupa {
-	string groupName;
-	string specialization;
-	vector <Student*> students;
-public:
-	Grupa();
-	Grupa(string groupName, string specialization);
-	void setGroupName(string groupName);
-	string getGroupName();
-	void setSpecialization(string specialization);
-	string getSpecialization();
-	void addStudent(Student* student);
-	void outPutStudent(string groupName, string specialization);
-	Grupa(const Student& student);
-	~Grupa();
+    string groupName;
+    string specialization;
+    vector<Student*> students;
 
-	friend ostream& operator << (std::ostream& os, const Grupa& student);
+public:
+    Grupa();
+    Grupa(string groupName, string specialization);
+    ~Grupa();
+
+    void setGroupName(string groupName);
+    string getGroupName();
+
+    void setSpecialization(string specialization);
+    string getSpecialization();
+
+    void addStudent(Student* student);
+    void outputStudent();
+
+    friend ostream& operator<<(ostream& os, const Grupa& grupa);
 };
