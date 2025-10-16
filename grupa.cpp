ca6cd536792d586c2cff670a93a6c77461c1ba90
Marian0954 <mariancompany7@gmail.com>
2025-10-16 21:15:35 +0300

    Update grupa.cpp

--- grupa.cpp
+++ grupa.cpp
@@ -1,45 +1,40 @@
 #include <iostream>
-#include "grupa.h"
-#include <string>
+#include "group.h"
+#include "student.h"
 using namespace std;
 
-Grupa::Grupa() {
-
-}
+Grupa::Grupa() {}
 
 Grupa::Grupa(string groupName, string specialization) {
-	this->groupName = groupName;
-	this->specialization = specialization;
+    this->groupName = groupName;
+    this->specialization = specialization;
 }
 
 Grupa::~Grupa() {
-
+ 
 }
 
-void Grupa::setGroupName(string groupName) { this->groupName = groupName; };
-string Grupa::getGroupName() { return groupName; };
-void Grupa::setSpecialization(string specialization) { this->specialization = specialization; };
-string Grupa::getSpecialization() { return specialization; };
+void Grupa::setGroupName(string groupName) { this->groupName = groupName; }
+string Grupa::getGroupName() { return groupName; }
 
-ostream& operator << (std::ostream& os, const Grupa& grupa) {
-	os << "GroupName: " << grupa.groupName << "Specialization: " << grupa.specialization;
-	for (int i = 0; i < grupa.students.size(); i++) {
-		cout << "student: " << i + 1 << grupa.students[i] << endl;
-	}
-	return os;
-}
-
-void Grupa::outPutStudent(string groupName, string specialization) {
-	cout << "GroupName: " << groupName << "Specialization: " << specialization;
-	for (int i = 0; i < students.size(); i++) {
-		cout << "student: " << i + 1 << *students[i] << endl;
-	}
-}
+void Grupa::setSpecialization(string specialization) { this->specialization = specialization; }
+string Grupa::getSpecialization() { return specialization; }
 
 void Grupa::addStudent(Student* student) {
-	students.push_back(student);
+    students.push_back(student);
 }
 
-Grupa::Grupa(const Student& student) {
-	
+void Grupa::outputStudent() {
+    cout << "GroupName: " << groupName << "\tSpecialization: " << specialization << endl;
+    for (size_t i = 0; i < students.size(); i++) {
+        cout << "\tStudent " << i + 1 << ": " << *students[i] << endl;
+    }
+}
+
+ostream& operator<<(ostream& os, const Grupa& grupa) {
+    os << "GroupName: " << grupa.groupName << "\tSpecialization: " << grupa.specialization << endl;
+    for (size_t i = 0; i < grupa.students.size(); i++) {
+        os << "\tStudent " << i + 1 << ": " << *grupa.students[i] << endl;
+    }
+    return os;
 }
