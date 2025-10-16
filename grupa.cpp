f28ba2d3f370f0eee78f13c4d49fe3b0289786ed
Marian0954 <mariancompany7@gmail.com>
2025-10-16 15:32:27 +0300

    Update grupa.cpp

--- grupa.cpp
+++ grupa.cpp
@@ -7,10 +7,9 @@ Grupa::Grupa() {
 
 }
 
-Grupa::Grupa(string groupName, string specialization, string ListOfStudents) {
+Grupa::Grupa(string groupName, string specialization) {
 	this->groupName = groupName;
 	this->specialization = specialization;
-	this->ListOfStudents = ListOfStudents;
 }
 
 Grupa::~Grupa() {
@@ -21,12 +20,26 @@ void Grupa::setGroupName(string groupName) { this->groupName = groupName; };
 string Grupa::getGroupName() { return groupName; };
 void Grupa::setSpecialization(string specialization) { this->specialization = specialization; };
 string Grupa::getSpecialization() { return specialization; };
-void Grupa::setListOfStudents(string ListOfStudents) { this->ListOfStudents = ListOfStudents; };
-string Grupa::getListOfStudents() { return ListOfStudents; };
 
-ostream& operator << (std::ostream& os, const Grupa& obj) {
-	os << "GroupName: " << obj.groupName << "Specialization: " << obj.specialization << "ListOfStudents: "
-		<< obj.ListOfStudents;
+ostream& operator << (std::ostream& os, const Grupa& grupa) {
+	os << "GroupName: " << grupa.groupName << "Specialization: " << grupa.specialization;
+	for (int i = 0; i < grupa.students.size(); i++) {
+		cout << "student: " << i + 1 << grupa.students[i] << endl;
+	}
 	return os;
 }
 
+void Grupa::outPutStudent(string groupName, string specialization) {
+	cout << "GroupName: " << groupName << "Specialization: " << specialization;
+	for (int i = 0; i < students.size(); i++) {
+		cout << "student: " << i + 1 << *students[i] << endl;
+	}
+}
+
+void Grupa::addStudent(Student* student) {
+	students.push_back(student);
+}
+
+Grupa::Grupa(const Student& student) {
+	
+}
